6b2b62a8b0e20e6159812742d2697499130e0f1c
Nitin10cd <nsaxenacse@gmail.com>
2025-09-16 10:12:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config();
+const app = express();
+
+// middlewares 
+
+
+//  main app runner 
+app.listen("");
